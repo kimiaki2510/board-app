5e621f4188f2df7f8ad240fb61e930d332e25379
kimiaki2510 <153046sk@gmail.com>
2020-10-01 23:31:37 +0900

    categories component

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,5 +1,14 @@
 <template>
+  <v-container>
+  </v-container>
 </template>
 
 <script>
+export default {
+  name: 'Categories',
+
+  data: () => ({
+  }),
+}
+
 </script>
--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="categories">
+    <Categories/>
   </div>
 </template>
 
