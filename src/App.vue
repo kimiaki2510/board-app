9eb4d85f2717a7bc37a49a1cc9a717279607e151
kimiaki2510 <153046sk@gmail.com>
2020-10-02 20:20:29 +0900

    board form

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,30 @@
 <template>
-  <v-card>
-    <v-toolbar
-      dark
-      prominent
-      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-toolbar-title>Your Board</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-toolbar>
-  </v-card>
+  <v-app>
+    <v-card>
+      <v-toolbar
+        dark
+        prominent
+        src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+      >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>Your Board</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </v-card>
+    <Boards/>
+  </v-app>
 </template>
+
 <script>
+import Boards from './components/Boards.vue'
 export default {
   name: 'App',
   components: {
+    Boards
   },
-
   data: () => ({
     //
   }),
--- src/components/Boards.vue
+++ src/components/Boards.vue
@@ -1,6 +1,18 @@
 <template>
-  <v-container>
-  </v-container>
+  <v-flex>
+    <v-card>
+      <v-card-text>
+        <v-form>
+          <v-text-field v-model="title" label="Title" />          
+        </v-form>
+        <v-card-actions>
+          <v-btn class="mx-2" fab dark color="indigo" @click="addList">
+            <v-icon>mdi-plus</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card-text>
+    </v-card>
+  </v-flex>
 </template>
 
 <script>
