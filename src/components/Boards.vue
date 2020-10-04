6c67996d5aee91d72e9146a3598bf2729c9ef852
kimiaki2510 <153046sk@gmail.com>
2020-10-04 15:07:40 +0900

    status

--- src/components/Boards.vue
+++ src/components/Boards.vue
@@ -3,10 +3,10 @@
     <v-card>
       <v-card-text>
         <v-form>
-          <v-text-field v-model="title" label="Title" />          
+          <v-text-field v-model="title" label="Title" />
         </v-form>
         <v-card-actions>
-          <v-btn class="mx-2" fab dark color="indigo" @click="addList">
+          <v-btn class="mx-2" fab dark color="green" @click="addList">
             <v-icon>mdi-plus</v-icon>
           </v-btn>
         </v-card-actions>
