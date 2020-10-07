cec2175f98ac92f1e2496b5ddcb97df2cedb31a7
kimiaki2510 <153046sk@gmail.com>
2020-10-07 22:58:17 +0900

    form

--- src/views/Boards.vue
+++ src/views/Boards.vue
@@ -1,47 +1,29 @@
 <template>
-  <v-container>
-    <v-layout>
-      <v-flex>
-        <v-simple-table>
-          <template v-slot:default>
-            <thead>
-              <tr>
-                <th class="text-left">
-                  Title
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                v-for="boards in desserts"
-                :key="boards.title"
-              >
-                <td>{{ boards.title }}</td>
-              </tr>
-            </tbody>
-          </template>
-      </v-simple-table>
+  <v-flex>
+    <v-card>
+      <v-card-text>
+        <v-form>
+          <v-text-field v-model="title" label="title" />
+        </v-form>
         <v-card-actions>
-          <v-btn class="mx-2" fab dark color="green" @click="addList">
+          <v-btn class="mx-2" fab dark color="indigo" @click="addList">
             <v-icon>mdi-plus</v-icon>
           </v-btn>
         </v-card-actions>
-      </v-flex>
-    </v-layout>
-  </v-container>
+      </v-card-text>
+    </v-card>
+  </v-flex>
 </template>
 <script>
 export default {
   data(){
     return{
-      headers: [
-        { text: 'タイトル', value: 'title' }
-      ],
-      boards: [
-        {
-          title: 'Vue.js'
-        }
-      ]
+      title: '',
+    }
+  },
+  method: {
+    addList(){
+      
     }
   }
 }
