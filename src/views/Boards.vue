1345b5cbb5b86883530d889ee860a40a7f107661
kimiaki2510 <153046sk@gmail.com>
2020-10-10 18:34:24 +0900

    board create

--- src/views/Boards.vue
+++ src/views/Boards.vue
@@ -2,9 +2,6 @@
   <v-flex>
     <v-card>
       <v-card-text>
-        <v-form v-on:submit.prevent="addBoard">
-          <v-text-field v-model="title" label="title" />
-        </v-form>
         <v-card-actions>
           <v-btn class="mx-2" fab dark color="indigo" @click="addList">
             <v-icon>mdi-plus</v-icon>
@@ -16,19 +13,41 @@
       <h2>Boards一覧</h2>
       <ul>
         <li v-for="board in boards" v-bind:key="board.id">
-          <input type="checkbox" v-bind:checked="board.done">
+          <input type="checkbox" v-bind:checked="board.done"
+           v-on:change="toggleBoardStatus(board)">
           {{ board.title }}
         </li>
       </ul>
+      <form v-on:submit.prevent="addBoard">
+        <input type="text" v-model="newBoardTitle" placeholder="新しいボード">
+      </form>
     </div>
   </v-flex>
 </template>
 <script>
 export default {
+  data(){
+    return {
+      newBoardTitle: '',
+    }
+  },
   computed: {
     boards() {
       return this.$store.state.boards
-    }
+    },
+  },
+  methods: {
+    addBoard () {
+      this.$store.commit('addBoard', {
+        title: this.newBoardTitle,
+      })
+      this.newBoardTitle = ''
+    },
+    toggleBoardStatus (board) {
+      this.$store.commit('toggleBoardStatus', {
+        id: board.id
+      })
+    },
   }
 }
 </script>
